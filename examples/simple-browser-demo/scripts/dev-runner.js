a8b31827fbe06d45033f84e2ab014e63a507d7b8
Eralde <eralde.g@gmail.com>
2025-11-24 19:57:46 +0100

    (fix) examples/simple-browser-demo: properly pass PROXY_ADDR to vite

--- examples/simple-browser-demo/scripts/dev-runner.js
+++ examples/simple-browser-demo/scripts/dev-runner.js
@@ -1,6 +1,11 @@
 const {execSync} = require('child_process');
 const yargs = require('yargs/yargs');
 const {hideBin} = require('yargs/helpers');
+const fs = require('fs');
+const path = require('path');
+const {loadEnvFile} = require('node:process');
+
+const DOTENV = path.join(__dirname, '..', '.env');
 
 const argv = yargs(hideBin(process.argv))
   .option(
@@ -16,21 +21,31 @@ const argv = yargs(hideBin(process.argv))
   .argv;
 
 const runDev = () => {
-  const {proxyAddr} = argv;
+  if (fs.existsSync(DOTENV)) {
+    loadEnvFile(DOTENV);
+  }
 
+  let proxyAddr;
   let command = 'vite';
 
-  if (proxyAddr) {
-    // Pass PROXY_ADDR as an environment variable to the vite process
-    command = `PROXY_ADDR=${proxyAddr} ${command}`;
+  if (argv.proxyAddr) {
+    proxyAddr = argv.proxyAddr;
+
     console.log(`[Dev Runner]: Using proxy address from CLI: ${proxyAddr}`);
   } else {
-    console.log('[Dev Runner]: No proxy address specified via CLI, using PROXY_ADDR from .env as fallback');
+    proxyAddr = process.env.PROXY_ADDR;
+
+    console.log(`[Dev Runner]: No proxy address specified via CLI, using PROXY_ADDR from .env as fallback: ${proxyAddr}`);
+  }
+
+  if (!proxyAddr) {
+    console.warn('[Dev Runner]: device proxy address is not specified. Stopping...');
+    process.exit(0);
   }
 
   try {
     console.log(`[Dev Runner]: Running command: ${command}`);
-    execSync(command, {stdio: 'inherit'});
+    execSync(command, {stdio: 'inherit', env: {PROXY_ADDR: proxyAddr}});
   } catch (error) {
     console.error('[Dev Runner]: Vite development server failed:', error.message);
     process.exit(1);
