6a5809b521461103288c0a7c55733b67a2fd07b0
Eralde <eralde.g@gmail.com>
2025-12-30 11:53:41 +0100

    (chore) examples/sv-webcli: change default HTTP server port to 12345

--- examples/sv-webcli/env.defaults.ts
+++ examples/sv-webcli/env.defaults.ts
@@ -1,7 +1,7 @@
 // see .env.template for more details
 export const ENV_DEFAULTS = {
   SSH_PORT: 222,
-  HTTP_PORT: 8081,
+  HTTP_PORT: 12345,
   REMOTE_WWW_ROOT: '/opt/share/www/',
   TTYD_PORT: 7681,
   TTYD_SCRIPTS_DIR: '/opt/etc/ttyd/',
