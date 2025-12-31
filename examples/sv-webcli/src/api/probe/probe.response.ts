84ca09bc8900d8cdb496dc02576d7a140adaf265
Eralde <eralde.g@gmail.com>
2025-12-31 09:45:16 +0100

    (task) @api/: add service for the 'probe' command

---
+++ examples/sv-webcli/src/api/probe/probe.response.ts
@@ -0,0 +1,8 @@
+export interface ProbeResponse {
+  prefix_no?: boolean;
+  multiple?: boolean;
+  nameable?: boolean;
+  read_only?: boolean;
+  has_prompt?: boolean;
+  found: boolean;
+}
---
+++ examples/sv-webcli/src/api/probe/probe.schema.ts
@@ -0,0 +1,10 @@
+import {z} from 'zod';
+
+export const probeResponseSchema = z.object({
+  prefix_no: z.boolean().optional(),
+  multiple: z.boolean().optional(),
+  nameable: z.boolean().optional(),
+  read_only: z.boolean().optional(),
+  has_prompt: z.boolean().optional(),
+  found: z.boolean()
+});
